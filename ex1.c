94172d45b24f2d7481c7393067347f61431f43b0
nguyen hieu <nguyenhieuvb04@gmail.com>
2026-03-23 23:58:36 +0700

    Test program

--- ex1.c
+++ ex1.c
@@ -1,5 +1,48 @@
-#include <stdio.h> 
-int main (){
-    printf ("hello world");
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+// Display Linked List Elements Function
+void display(struct node *head)
+{
+    struct node *temp = head;
+    while (temp != NULL)
+    {
+        printf("%d -> ", temp->data);
+        temp = temp->next;
+    }
+    printf("NULL\n");
+};
+
+// Addback an Element Linked List Function
+struct node *addback(struct node *head, int data)
+{
+    struct node *newnode = (struct node *)malloc(sizeof(struct node));
+    newnode->data = data;
+    newnode->next = NULL;
+    if (head == NULL)
+        return newnode;
+    struct node *temp = head;
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+    temp->next = newnode;
+    return head;
+};
+
+int main()
+{
+    struct node *head = NULL;
+    head = addback(head, 1);
+    head = addback(head, 2);
+    head = addback(head, 3);
+
+    display(head);
     return 0;
 }
